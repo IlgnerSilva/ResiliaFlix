00d7bc06da25965a1c9a10224708c86029d88c28
IlgnerSilva <ilgnersilva@outlook.com>
2021-09-11 17:51:20 -0300

    Pesquisa de filme

--- javascript/teste/script.js
+++ javascript/teste/script.js
@@ -1,7 +1,19 @@
 var search = document.getElementById('search');
 var log = document.getElementById('print');
+let pedido = new XMLHttpRequest();
 
 search.addEventListener('keyup', function() {
-  log.innerHTML = "Pesquisando: " + this.value;
+    pedido.open("GET", `https://www.omdbapi.com/?t=${this.value}&apikey=18c57453`)
+    console.log(pedido)
+    log.innerHTML = "Pesquisando: ";
+    console.log(this.value)
+    pedido.onload = function(){
+      
+  }
   
+  
+  pedido.send();
 });
+
+
+
