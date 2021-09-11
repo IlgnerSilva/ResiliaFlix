8a3795f60947378e0f6a35639bead2a22e03d8c0
IlgnerSilva <ilgnersilva@outlook.com>
2021-09-11 18:10:10 -0300

    Arquivo pesquisa de filme corrigido

--- javascript/teste/script.js
+++ javascript/teste/script.js
@@ -5,10 +5,13 @@ let pedido = new XMLHttpRequest();
 search.addEventListener('keyup', function() {
     pedido.open("GET", `https://www.omdbapi.com/?t=${this.value}&apikey=18c57453`)
     console.log(pedido)
-    log.innerHTML = "Pesquisando: ";
     console.log(this.value)
+    
     pedido.onload = function(){
-      
+        let guarda = JSON.parse(pedido.responseText)
+        console.log(guarda)
+        log.innerHTML = "Pesquisando: " + guarda.Plot;
+        document.querySelector('img').setAttribute('src', guarda.Poster)
   }
   
   
