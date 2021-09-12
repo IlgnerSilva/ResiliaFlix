b7730466ced20d1e8173c6df770d52785c830334
viniciusr-cmd <vrs122@gmail.com>
2021-09-12 16:46:46 -0300

    index.html: adicionado tag p com id plot / adicionado erro de requisição no script

--- javascript/teste/script.js
+++ javascript/teste/script.js
@@ -2,22 +2,29 @@ var search = document.getElementById('search');
 var log = document.getElementById('print');
 let pedido = new XMLHttpRequest();
 
-search.addEventListener('keyup', function() {
-    pedido.open("GET", `https://www.omdbapi.com/?t=${this.value}&apikey=18c57453`)
-    console.log(pedido)
-    console.log(this.value.replaceAll(' ', '+'))
-    
-    pedido.onload = function(){
-        let guarda = JSON.parse(pedido.responseText)
-        console.log(guarda)
-        log.innerHTML = "Pesquisando: " + guarda.Plot;
-        let cria = document.createElement('img')
-        cria.setAttribute('src', guarda.Poster)
-        document.querySelector('body').prepend(cria)
-        
+search.addEventListener('input', function () {
+  pedido.open("GET", `https://www.omdbapi.com/?t=${this.value}&apikey=18c57453`)
+  console.log(pedido)
+  console.log(this.value.replaceAll(' ', '+'))
+  pedido.onerror = function(){
+    log.innerHTML = `Um erro ocorreu, por favor, tente novamente.`
   }
-  
-  
+
+  pedido.onload = function () {
+    if (search.value != '') {
+      let guarda = JSON.parse(pedido.responseText)
+      console.log(guarda)
+      log.innerHTML = "Pesquisando: " + guarda.Title;
+      let cria = document.createElement('img')
+      cria.setAttribute('src', guarda.Poster)
+      let sinopse = document.getElementById('plot')
+      sinopse.innerHTML = `Sinopse: ${guarda.Plot}`
+      document.querySelector('body').prepend(cria)
+    }
+
+  }
+
+
   pedido.send();
 });
 
