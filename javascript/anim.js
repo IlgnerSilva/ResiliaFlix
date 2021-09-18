0adcf5e02c95c0592f712197dba41066b669cafa
Vitória <vivi.sousa964@gmail.com>
2021-09-18 14:52:12 -0300

    Scroll de imagens/gradiente de cores(degradê)

---
+++ javascript/anim.js
@@ -0,0 +1,15 @@
+let GalleryCenter = document.getElementById('GalleryCenter');
+var count = 0;
+
+window.addEventListener('wheel',function(event){
+	if(event.deltaY < 0){
+		count -= 12;
+		console.log(count)
+		GalleryCenter.style.transform = 'translate(-50%, -50%) perspective(1000px) rotateY('+count+'deg)'
+	}
+	else if(event.deltaY > 0){
+		count += 12;
+		console.log(count)
+		GalleryCenter.style.transform = 'translate(-50%, -50%) perspective(1000px) rotateY('+count+'deg)'
+	}
+});
