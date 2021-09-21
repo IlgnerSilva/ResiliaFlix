1098021b2af71f01abd47d4daa99e0247c56922d
IlgnerSilva <ilgnersilva@outlook.com>
2021-09-21 04:03:13 -0300

    Comentando código

--- javascript/Login e Modal/modal.js
+++ javascript/Login e Modal/modal.js
@@ -14,3 +14,20 @@ document.querySelector('.fechar-modal').addEventListener('click', () => {
     document.querySelector('.input-modal').value = ''
     modalContainer.style.display = 'none'
 })
+
+// let posterFilmes = ['tt0198781', 'tt0414387', 'tt1951264', 'tt0434409', 'tt6723592', 'tt0106062', 'tt10838180', 'tt0102926', 'tt0266697', 'tt0213149', 'tt0068646', 'tt0848228' ]
+// let pedido = new XMLHttpRequest();
+
+// let array = document.querySelectorAll('.card').forEach((id, item)=>{
+//     pedido.open("GET", `https://www.omdbapi.com/?t=2021&apikey=18c57453`)
+//     console.log(pedido)
+//     pedido.onload = function () {
+//         let guarda = JSON.parse(pedido.responseText)
+
+//           console.log('foi')
+//           id.innerHTML = `<img src="${guarda.Poster}"></img>`
+      
+//         }
+//     pedido.send();
+// })
+
