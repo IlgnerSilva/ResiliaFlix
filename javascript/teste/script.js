45dd6156853841ac26ced9ff56740f3b83f97774
IlgnerSilva <ilgnersilva@outlook.com>
2021-09-20 22:28:42 -0300

    pesquisa filmes

--- javascript/teste/script.js
+++ javascript/teste/script.js
@@ -1,11 +1,12 @@
 var search = document.getElementById('search');
 var log = document.getElementById('print');
+let key = '18c57453'
 let pedido = new XMLHttpRequest();
 
 search.addEventListener('input', function () {
-  pedido.open("GET", `https://www.omdbapi.com/?t=${this.value}&apikey=18c57453`)
+  pedido.open("GET", `https://www.omdbapi.com/?t=${this.value.replaceAll(' ', '+')}&apikey=18c57453`)
   console.log(pedido)
-  console.log(this.value.replaceAll(' ', '+'))
+  console.log(`https://www.omdbapi.com/?t=${this.value.replaceAll(' ', '+')}&apikey=${key}`)
   pedido.onerror = function () {
     log.innerHTML = `Um erro ocorreu, por favor, tente novamente.`
   }
@@ -13,13 +14,14 @@ search.addEventListener('input', function () {
   pedido.onload = function () {
     if (search.value != '') {
       let guarda = JSON.parse(pedido.responseText)
-      console.log(guarda)
-      log.innerHTML = "Pesquisando: " + guarda.Title;
-      let cria = document.createElement('img')
-      cria.setAttribute('src', guarda.Poster)
-      let sinopse = document.getElementById('plot')
-      sinopse.innerHTML = `Sinopse: ${guarda.Plot}`
-      document.querySelector('body').prepend(cria)
+      if(guarda.Poster == 'N/A' || guarda.Poster == undefined){
+
+      }else{
+        console.log('e agora',guarda.Poster)
+        log.innerHTML = "Pesquisando: " + guarda.Title;
+        document.querySelector('img').setAttribute('src', guarda.Poster)
+        document.querySelector('#plot').innerHTML = `Sinopse: ${guarda.Plot}`
+      }
     }
   }
   pedido.send();
