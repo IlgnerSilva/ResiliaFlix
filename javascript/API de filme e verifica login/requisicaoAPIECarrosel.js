acc3c598f6cca33e3c05d472e7e2fb3f54817b0d
IlgnerSilva <ilgnersilva@outlook.com>
2021-09-22 17:11:13 -0300

    Hover de informções no carrosel

--- javascript/API de filme e verifica login/requisicaoAPIECarrosel.js
+++ javascript/API de filme e verifica login/requisicaoAPIECarrosel.js
@@ -1,7 +1,7 @@
 let posterFilmes = ['tt0198781', 'tt0414387', 'tt1951264', 'tt0434409', 'tt6723592', 'tt2911666', 'tt8228288', 'tt0102926', 'tt0266697', 'tt0213149', 'tt0068646', 'tt0848228' ]
 
-let divPostersss = document.querySelectorAll('.postersss');
-let postersss = document.querySelectorAll('.postersss')
+let divPostersss = document.querySelectorAll('.posters-filme');
+
 
 for(let i in divPostersss){    
     $.ajax({
@@ -11,20 +11,16 @@ for(let i in divPostersss){
     complete: function (dados) {
         response = dados.responseJSON;
         let criaImage = document.createElement('img')
-        let informacoes = document.createElement('div')
-        criaImage.classList.add(`teste${i}`)
-        informacoes.classList.add('teste')
-        informacoes.innerHTML = `<p class='absolute' id='${posterFilmes[i]}'>${response.Plot}</p>`
+        let pSinopse = document.createElement('p')
+        let titleFilme = document.createElement('h2')
+        pSinopse.classList = 'pSinopse'
+        titleFilme.innerHTML = response.Title
+        pSinopse.innerHTML = response.Plot
         criaImage.setAttribute('src', response.Poster)
-        criaImage.setAttribute('onclick', `cliquei(${posterFilmes[i]})`)
-        postersss[i].appendChild(informacoes)
+        divPostersss[i].append(titleFilme)
         divPostersss[i].append(criaImage)
+        divPostersss[i].append(pSinopse)
     }
 
 });
-}
-
-function cliquei(num){
-
-        alert(document.getElementById(num))
 }
